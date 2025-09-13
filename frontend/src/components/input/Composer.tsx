78eae02494bb16939f3adc05b57cceff467317b8
zhaju <juleoan123@gmail.com>
2025-09-13 16:16:33 -0400

    fixed button color

--- frontend/src/components/input/Composer.tsx
+++ frontend/src/components/input/Composer.tsx
@@ -73,11 +73,11 @@ export const Composer = ({
 
       {/* Disabled Message */}
       {disabled && (
-        <div className="mb-3 p-3 bg-yellow-50 border border-yellow-200 
-                        dark:bg-yellow-900/20 dark:border-yellow-800 
-                        rounded-lg text-yellow-800 dark:text-yellow-200 text-sm">
+        <div className="mb-3 p-3 bg-yellow-300 text-black 
+                        dark:bg-yellow-800 dark:text-yellow-200
+                        rounded-lg text-sm">
           <div className="flex items-center space-x-2">
-            <svg className="w-4 h-4 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+            <svg className="w-4 h-4 flex-shrink-0 text-black dark:text-yellow-200" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
             </svg>
             <span>{disabledMessage}</span>
